fbd51c2638b75de7d08df4eaab6348c05832ad70
JUN <chaeyjs827@gmail.com>
2021-06-22 23:48:40 +0900

    2021.06.22 commit

---
+++ src/test/java/com/kpsec/FirstTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kpsec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
