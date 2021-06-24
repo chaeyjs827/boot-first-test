d3de2a8458c2ef03516238272365266c9c6b284b
JUN <chaeyjs827@gmail.com>
2021-06-24 15:39:27 +0900

    2021.06.24 commit

--- src/main/java/com/kpsec/test/repository/AccountRepository.java
+++ src/main/java/com/kpsec/test/repository/AccountRepository.java
@@ -12,15 +12,25 @@ import com.kpsec.test.model.entity.Account;
 public interface AccountRepository extends JpaRepository<Account, String> {
 
 //	@Query(value = "SELECT account_no as accountNo, account_name as accountName FROM account WHERE branch_code = :branchCode", nativeQuery = true)
-	@Query(value = "SELECT sub.transaction_year AS YEAR, MAX(sub.account_name) AS name , MAX(sub.account_no) AS accNo,  MAX(sub.sumAmt) AS sumAmt\n" + 
-			"FROM (\n" + 
-			"SELECT YEAR(t.transaction_date) AS transaction_year, t.account_no, acc.account_name, SUM(t.transaction_amount-t.transaction_fee) AS sumAmt\n" + 
-			"FROM account acc\n" + 
-			"JOIN transaction_history t ON acc.account_no = t.account_no\n" + 
-			"WHERE t.transaction_voidYN = 'N'\n" + 
-			"GROUP BY t.account_no, YEAR(t.transaction_date)\n" + 
-			") AS sub\n" + 
-			"GROUP BY sub.transaction_year;", nativeQuery = true)
+	@Query(value = "select transaction_year as year,account_name as name, account_no as accNo, a.sumAmt as sumAmt\n" + 
+			"from \n" + 
+			"   (SELECT YEAR(t.transaction_date) AS transaction_year, t.account_no, acc.account_name, SUM(t.transaction_amount-t.transaction_fee) AS sumAmt\n" + 
+			"   FROM account acc\n" + 
+			"   JOIN transaction_history t ON acc.account_no = t.account_no\n" + 
+			"   WHERE t.transaction_voidYN = 'N'\n" + 
+			"   GROUP BY t.account_no, YEAR(t.transaction_date)\n" + 
+			"   ) as a ,\n" + 
+			"   (SELECT max(sumAmt) as sumAmt\n" + 
+			"   FROM (\n" + 
+			"      SELECT YEAR(t.transaction_date) AS transaction_year, t.account_no, acc.account_name, SUM(t.transaction_amount-t.transaction_fee) AS sumAmt\n" + 
+			"      FROM account acc\n" + 
+			"      JOIN transaction_history t ON acc.account_no = t.account_no\n" + 
+			"      WHERE t.transaction_voidYN = 'N'\n" + 
+			"      GROUP BY t.account_no, YEAR(t.transaction_date)\n" + 
+			"      ) as sub\n" + 
+			"   group by transaction_year) as b \n" + 
+			"where b.sumAmt = a.sumAmt\n" + 
+			"order by transaction_year;", nativeQuery = true)
 //    @Query(value = "SELECT * FROM account", nativeQuery = true)
     List<TopCustomer> getAccountByBranchCode(@Param("branchCode") String branchCode);
 
