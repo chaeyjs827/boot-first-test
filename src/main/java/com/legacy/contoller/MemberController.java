77bf46155ee3f531614bf7b900526d9f452f87fd
JUN <chaeyjs827@gmail.com>
2021-07-02 22:42:08 +0900

    2021.07.02 commit

---
+++ src/main/java/com/legacy/contoller/MemberController.java
@@ -0,0 +1,11 @@
+package com.legacy.contoller;
+
+import org.springframework.stereotype.Controller;
+
+import lombok.AllArgsConstructor;
+
+@Controller
+@AllArgsConstructor
+public class MemberController {
+
+}
