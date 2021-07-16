277ac5f24b34a212ce5e112268104e938f67266f
JUN <chaeyjs827@gmail.com>
2021-07-16 23:21:25 +0900

    2021.07.16 sample structure for observer pattern

---
+++ src/main/java/com/legacy/observer/SampleObserver.java
@@ -0,0 +1,15 @@
+package com.legacy.observer;
+
+/*
+ * 프로젝트 개발 때 옵저버 패턴을 사용할 부분임
+ *
+ * 가장 쉬운 예제는 유투버는 Subject, 구독자는 Observer로 볼 수 있음
+ * 
+ * 옵저버 패턴은 한 객체의 상태가 바뀌면 그 객체에 의존하는 다른 객체들한테 연락이 가고,
+ * 자동으로 내용이 갱신되는 방식으로 일대다 의존성을 정의한다.
+ * -> Subject를 참조하는 수 많은 Observer들이 Subject의 수정이 생기면 자동으로 갱신됨
+ */
+
+public class SampleObserver {
+
+}
